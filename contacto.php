764396fa15d74987611e9080e04e2ec46bc6742d
Yan-art-lang <79186582+Yan-art-lang@users.noreply.github.com>
2021-02-16 23:59:13 -0600

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,11 @@
+<?php
+	echo 'Esta es la información introducida en el formulario: <br>';
+	
+	 $_POST['Nombre'].'<br>';
+	 $_POST['Correo'].'<br>';
+	 $_POST["Teléfono"].'<br>';
+	 $_POST['mensaje'].'<br>';
+	 echo $Nombre;
+	
+	
+?>
